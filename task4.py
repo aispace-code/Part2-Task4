7e0057c38a611074ccddbdd0d79be5ce7d4df901
aispace-code <aisuluu998@gmail.com>
2019-11-13 17:26:20 +0600

    «Aisuluu»

---
+++ task4.py
@@ -0,0 +1,7 @@
+num_hours1 = int(input('Number of hours '))
+num_min1 = int(input('Number of minutes '))
+num_sec1 = int(input('Number of seconds '))
+num_hours2 = int(input('Number of hours '))
+num_min2 = int(input('Number of minutes '))
+num_sec2 = int(input('Number of seconds '))
+print(((num_hours2-num_hours1)*3600)+((num_min2-num_min1)*60)+(num_sec2-num_sec1))
